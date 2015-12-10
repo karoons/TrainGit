cd299f0fe3d298237a5709b467986c83fbc0bb7a
karoons <k.sillapapam@gmail.com>
2015-12-10 16:32:51 +0700

    Revert "Edit Test.java"
    This reverts commit 4b78b04c9d3bb42b72a68247897591e2d94ec6aa.

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,6 @@ class  Test
 {
 	public static void main(String[] args) 
 	{
-//		System.out.println("Hello World!");
-			System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
